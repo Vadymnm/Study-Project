c0228547f058ed174ad977f1dbc64250af625b56
Vadym <telcom.odessa@gmail.com>
2024-03-29 19:49:02 +0300

    Initial

--- main.py
+++ main.py
@@ -24,12 +24,23 @@ def test2(a,b,c):
     print(a,b,c)
 # -----------------------
 def test3(a,b,c):
+    print(a,b,c)
     c = 'String'
+    b = True
     print(a, b, c)
 # -----------------------
-test()
+def test4():
+    global b, c
+    print(a, b, c)
+    c = 'String'
+    b = True
+    print(a, b, c)
+
 a = 10
 b = 15
 c = 20
+test()
 test2(a,b,c)
-test3(1,b,c)
+test3(4,b,c)
+test4()
+print(a, b, c)
