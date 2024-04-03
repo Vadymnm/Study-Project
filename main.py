1c5e9386caf00286c6ecaa3369e3c4ba58ed7de7
Vadym <telcom.odessa@gmail.com>
2024-04-03 22:29:59 +0300

    Initial

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
-def test():
-    a = 5
-    b = 8
-    print(a,b)
-# -----------------------
-def test2(a,b,c):
-    print(a,b,c)
-# -----------------------
-def test3(a,b,c):
-    print(a,b,c)
-    c = 'String'
-    b = True
-    print(a, b, c)
-# -----------------------
-a = 10
-b = 15
-c = 20
-test()
-test2(a,b,c)
-test3(4,b,c)
+def test_1(a,b=25,*args,**kwargs):
+    print('a = ',a,'b = ',b)
+    print('type args = ',type(args))
+    print('args = ',args)
+    for i, arg in enumerate(args):
+        print('позиционный параметр:  i = ',i, 'arg = ',arg)
 
+    print('type args = ',type(kwargs))
+    print(kwargs)
+    for key, value in kwargs.items():
+        print('именованный параметр:  key = ',key, 'value = ',value)
+
+test_1(3,4,6,8,9,dog='gav gav',cat='мяу мяу')
+print('-----------------------------------------------')
+test_1(3,5,77,88,99,dog='gav gav',cat='мяу мяу')
+print('-----------------------------------------------')
+test_1(a=3,dog='gav gav',cat='мяу мяу')
+
+#  *****************************************************************************
+#  ****** FACTORIAL ********
+
+print( '****** FACTORIAL - 4 варианта  ********' )
+print('Введите  число')
+n = int(input())
+print(n)
+#  ----------------------------------------
+def fact(n):
+    if n ==1:
+#        result = 1
+        return 1
+    result = fact(n-1)
+    return n * result
+
+print('рекурсия', fact(n))
+# ==============================
+i = 1
+fact = 1
+while i <= n:
+    fact = fact*i
+    i += 1
+print('while', fact)
+# ===============================
+fact = 1
+for i in range(1,n+1):
+    fact = fact*i
+print('for  ', fact)
+# ===============================
+import math
+fact =  math.factorial(n)
+print('Lib  ', fact)
+# ===============================
