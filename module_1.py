d15036481afe7a465930f4de82bbfb067c76d186
Vadym <telcom.odessa@gmail.com>
2024-04-13 20:25:06 +0300

    Initial

---
+++ module_1.py
@@ -0,0 +1,7 @@
+print('module name is:',__name__)
+variable_1 = 'Hello, world'
+def function_1():
+    print('Hello, world - this is function_1')
+def function_2():
+    print('Hello, world - this is function_2')
+
