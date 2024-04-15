369a17934d03e45c7653a10ad9c678e6e2d182c1
Vadym <telcom.odessa@gmail.com>
2024-04-15 19:21:28 +0300

    Initial

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import module_1
-import module_2
 
-print(dir(module_1))
-print(dir(module_2))
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
 
-print(module_1.variable_1)
-module_1.function_1()
-module_1.function_2()
-
-print('module name is:',__name__)
+test_function()
+print('-------------------------------')
+#inner_function()
