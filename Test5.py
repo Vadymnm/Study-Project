ebcbc650a2c054fc0f4c456050e6bd3c019e5e7c
Vadym <telcom.odessa@gmail.com>
2024-04-20 17:03:00 +0300

    Initial

---
+++ Test5.py
@@ -0,0 +1,24 @@
+class House:
+
+    def __init__(self):
+        self.numberOfFloors = 10
+        self.FloorNo = 0
+
+    def printFloor(self):
+        print('Текущий Этаж № ', self.FloorNo)
+
+
+my_house = House()
+
+print('==================================')
+print(' --- UP ---')
+my_house.FloorNo = 0
+while my_house.FloorNo < my_house.numberOfFloors:
+    my_house.FloorNo += 1
+    my_house.printFloor()
+
+print('==================================')
+print(' --- DOWN ---')
+while my_house.FloorNo >= 1:
+    my_house.printFloor()
+    my_house.FloorNo -= 1
