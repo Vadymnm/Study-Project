9ea7ffecf711c59ebc23bce5dbaa422aee6d4dc4
Vadym <telcom.odessa@gmail.com>
2024-04-21 22:56:11 +0300

    Initial

--- Test5.py
+++ Test5.py
@@ -1,3 +1,28 @@
+# class House:
+#
+#     def __init__(self):
+#         self.numberOfFloors = 10
+#         self.FloorNo = 0
+#
+#     def printFloor(self):
+#         print('Текущий Этаж № ', self.FloorNo)
+#
+#
+# my_house = House()
+#
+# print('==================================')
+# print(' --- UP ---')
+# my_house.FloorNo = 0
+# while my_house.FloorNo < my_house.numberOfFloors:
+#     my_house.FloorNo += 1
+#     my_house.printFloor()
+#
+# print('==================================')
+# print(' --- DOWN ---')
+# while my_house.FloorNo >= 1:
+#     my_house.printFloor()
+#     my_house.FloorNo -= 1
+
 class House:
 
     def __init__(self):
@@ -7,18 +32,19 @@ class House:
     def printFloor(self):
         print('Текущий Этаж № ', self.FloorNo)
 
+    def setNewNumberOfFloors(self, floors):
+        self.comtent = floors
+        print(self.numberOfFloors)
+
+
 
 my_house = House()
 
 print('==================================')
-print(' --- UP ---')
-my_house.FloorNo = 0
-while my_house.FloorNo < my_house.numberOfFloors:
-    my_house.FloorNo += 1
-    my_house.printFloor()
+my_house.setNewNumberOfFloors(10)
 
-print('==================================')
-print(' --- DOWN ---')
-while my_house.FloorNo >= 1:
-    my_house.printFloor()
-    my_house.FloorNo -= 1
+# print('==================================')
+# print(' --- DOWN ---')
+# while my_house.FloorNo >= 1:
+#     my_house.printFloor()
+#     my_house.FloorNo -= 1
---
+++ Test5_1.py
@@ -0,0 +1,19 @@
+
+class House:
+
+    def __init__(self):
+        self.numberOfFloors = 0
+    #    self.FloorNo = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print('New number of Floors = ',self.numberOfFloors)
+
+
+
+my_house = House()
+
+print('Initial number of Floors = ',my_house.numberOfFloors)
+print('==================================')
+my_house.setNewNumberOfFloors(15)
+
