9aa6b4d382c2f13b7e6b590eb2ba6d1f68ee9ea6
Vadym <telcom.odessa@gmail.com>
2024-05-07 15:06:21 +0300

    Initial

---
+++ Package 6/Test6-1.py
@@ -0,0 +1,51 @@
+
+class Car:
+    price = 1000000
+    horse_power = 99
+    def __init__(self,name):
+        self.name = name
+
+    def horse_powers(self):
+        self.horse_power = 88
+        print(self.__class__.__name__, self.name,':','horse power =',self.horse_power )
+        return(self.horse_power)
+    def __str__(self):
+        return '{} {} цена =  {}, horse_power = {}'.format(
+            self.__class__.__name__, self.name, self.price, self.horse_power)
+
+class Nissan(Car):
+    horse_power = 125
+    price = 500000
+    def horse_powers(self):
+        self.horse_power = 77
+        print(self.__class__.__name__, self.name,':','horse power =',self.horse_power )
+        return(self.horse_power)
+
+class KIA (Car):
+#    horse_power = 110
+    price = 2500000
+    def horse_powers(self):
+        print(self.__class__.__name__, self.name, ':', 'horse power =', self.horse_power)
+
+
+my_patrol = Nissan(name='Patrol')
+my_rio = KIA(name='Rio')
+
+print('===============================================')
+print('-----параметр horse_power выбирается из атрибутов  наследника класса Car - Nissan')
+print()
+print(my_patrol)
+print('===============================================')
+print('----параметр horse_power определяется  функцией horse_powers() из наследника класса Car - Nissan')
+print()
+my_patrol.horse_powers()
+print(my_patrol)
+
+print('***********************************************')
+print('----параметр horse_power выбирается из атрибутов  родительского класса Car')
+print()
+
+print(my_rio)
+my_rio.horse_powers()
+
+print('===============================================')
--- Test5_3.py
+++ Test5_3.py
@@ -1,14 +1,37 @@
-class Building:
-    total = 0
-    def __init__(self):
-        Building.total += 1
+# class Building:
+#     total = 0
+#     def __init__(self):
+#         Building.total += 1
+#
+# result = []
+# n = 5
+# while len(result) < n:
+#     new_building_No = Building()
+#     result.append(new_building_No)
+#
+# for i in range (0,n):
+#     print('new_building_No=',i,result[i])
 
-result = []
-n = 40
-while len(result) < n:
-    new_building_No = Building()
-    result.append(new_building_No)
 
-for i in range (0,n):
-    print('new_building_No=',i,result[i])
+import sys
+from termcolor import colored, cprint
 
+
+text = colored('Hello, My World!', 'red', attrs=['reverse', 'blink'])
+cprint('Hello, Our World!', 'green', 'on_red')
+cprint(text, 'green', 'on_red')
+
+
+def print_red_on_cyan(x):
+    return cprint(x, 'red', 'on_cyan')
+
+
+print_red_on_cyan('Hello, World!')
+print_red_on_cyan('Hello, Universe!')
+
+for i in range(10):
+    cprint(i, 'red', end=' ')
+print()
+print('===================')
+
+cprint("Attention!  !!!!", 'red', attrs=['bold'], file=sys.stderr)
